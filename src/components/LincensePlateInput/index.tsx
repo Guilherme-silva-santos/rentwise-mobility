df348a0ca1b012f83f10402ae04d526736b560f7
Guilherme-silva-santos <guilhermesilvasantosjnc@gmail.com>
2024-03-20 13:26:42 -0300

    validation lisenceplate input

--- src/components/LincensePlateInput/index.tsx
+++ src/components/LincensePlateInput/index.tsx
@@ -1,22 +1,29 @@
+/* eslint-disable react/display-name */
 import { Text, TextInput, TextInputProps, View } from 'react-native'
 import { styles } from './styles'
 import theme from '../../theme'
+import { forwardRef } from 'react'
 
 type Props = TextInputProps & {
   label: string
 }
 
-export function LincensePlateInput({ label, ...rest }: Props) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.label}>{label}</Text>
-      <TextInput
-        style={styles.input}
-        maxLength={7}
-        autoCapitalize="characters"
-        placeholderTextColor={theme.COLORS.GRAY_400}
-        {...rest}
-      />
-    </View>
-  )
-}
+const LicensePlateInput = forwardRef<TextInput, Props>(
+  ({ label, ...rest }, ref) => {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.label}>{label}</Text>
+        <TextInput
+          ref={ref}
+          style={styles.input}
+          maxLength={7}
+          autoCapitalize="characters"
+          placeholderTextColor={theme.COLORS.GRAY_400}
+          {...rest}
+        />
+      </View>
+    )
+  },
+)
+
+export { LicensePlateInput }
--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,42 +1,78 @@
-import { TextInput, View } from 'react-native'
-import { styles } from './styles'
-import { Header } from '../../components/Header'
-import { LincensePlateInput } from '../../components/LincensePlateInput'
+import {
+  TextInput,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+} from 'react-native'
+import { useRef, useState } from 'react'
+
+import { LicensePlateInput } from '../../components/LincensePlateInput'
 import { TextAreaInput } from '../../components/TextareaInput'
 import { Button } from '../../components/Button/button'
-import { useRef } from 'react'
+import { Header } from '../../components/Header'
+
+import { styles } from './styles'
+import { licensePlateValidate } from '../../utils/licensePlateValidade'
+
+const keyboardAvoidingViewBehavior =
+  Platform.OS === 'android' ? 'height' : 'position'
 
 export function Departure() {
+  const [description, setdescription] = useState('')
+  const [lisenceplate, setLincesePlate] = useState('')
+
   // criando referência do input para usar o botão do teclado para direcionar o user para o proximo input
   const descriptionRef = useRef<TextInput>(null)
+  const licensePlateRef = useRef<TextInput>(null)
 
+  // cadastro
   function handleDepartureRegister() {
-    console.log('ok')
+    // se o padrão da placa não bater
+    if (!licensePlateValidate(lisenceplate)) {
+      licensePlateRef.current?.focus()
+      return Alert.alert(
+        'Placa Inválida',
+        'A placa é inválida. Por favor informe a placa correta do veículo',
+      )
+    }
   }
 
   return (
     <View style={styles.container}>
       <Header title="Saída" />
 
-      <View style={styles.content}>
-        <LincensePlateInput
-          label="placa do veículos"
-          placeholder="BRA1234"
-          // para que p user seja direcionado para o input através da ref
-          onSubmitEditing={() => descriptionRef.current?.focus()}
-          returnKeyType="next"
-        />
-        <TextAreaInput
-          ref={descriptionRef}
-          label="Finalidade"
-          placeholder="Vou utilizar o veículo para..."
-          // para que no teclado haja um botão de submitting
-          onSubmitEditing={handleDepartureRegister}
-          returnKeyType="send"
-          blurOnSubmit
-        />
-        <Button title="Registrar Saída" onPress={handleDepartureRegister} />
-      </View>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={keyboardAvoidingViewBehavior}
+      >
+        <ScrollView>
+          <View style={styles.content}>
+            <LicensePlateInput
+              ref={licensePlateRef}
+              label="placa do veículos"
+              placeholder="BRA1234"
+              // para que p user seja direcionado para o input através da ref, ele chama o proximo input
+              onSubmitEditing={() => descriptionRef.current?.focus()}
+              returnKeyType="next"
+              onChangeText={setLincesePlate}
+            />
+            <TextAreaInput
+              ref={descriptionRef}
+              label="Finalidade"
+              placeholder="Vou utilizar o veículo para..."
+              // chama a função para fazer o submit do form
+              onSubmitEditing={handleDepartureRegister}
+              returnKeyType="send"
+              // como é um multline a odefaultseria quebrar as linhas, mas com o bluroncubmit ele "esconde" essa funcionalidade
+              blurOnSubmit
+              onChangeText={setdescription}
+            />
+            <Button title="Registrar Saída" onPress={handleDepartureRegister} />
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </View>
   )
 }
